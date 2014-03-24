5f83ebd8e3375f879c5beff259eefd78312cf1a7
taguro <tagurodevelop@yahoo.co.jp>
2014-03-24 18:59:45 +0900

    ダミーのゲーム用のプロパティと基本機能部分の分離

---
+++ Plock/GameMethodProperty.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Plock
+{
+    using GameData = DummyGameProject.GameData;
+
+
+
+    class GameMethodProperty:baseGameMethodProperty
+    {
+        public new static Dictionary<string, Method> getDoMethodDictionary()
+        {
+            Dictionary<string, Method> methodDictionary = baseGameMethodProperty.getDoMethodDictionary();
+            //////////TODO:２．ゲームで使うメソッドのクラスを辞書に登録する/////////////
+            methodDictionary.Add("xを1増やす", new AddX());
+            methodDictionary.Add("xを1減らす", new MinusX());
+            methodDictionary.Add("ゲームの初期化", new Constructor());//コンストラクタもここで宣言する
+            return methodDictionary;
+        }
+
+        public class AddX : Method    //ダミーのゲームのメソッド
+        {
+            public override GameData execute(GameData game)
+            {
+                game.addX();
+                return game;
+            }
+        }
+        public class MinusX : Method    //ダミーのゲームのメソッド
+        {
+            public override GameData execute(GameData game)
+            {
+                game.minusX();
+                return game;
+            }
+        }
+
+        public class Constructor : Method    //ダミーのゲームのメソッド
+        {
+            public override GameData execute(GameData game)//GUIのプログラムから呼ぶ場合
+            {
+                return new GameData(3, 3);
+            }
+
+            public GameData execute()//最初の初期化
+            {
+                return new GameData(3, 3);
+            }
+        }
+
+
+
+
+
+        //////////bool値を返すメソッドも大体同じ要領で登録する/////////////
+        public static Dictionary<string, IsMethod> getIsMethodDictionary()
+        {
+            Dictionary<string, IsMethod> methodDictionary = new Dictionary<string, IsMethod>();
+
+            methodDictionary.Add("xが2なら", new IsX2());
+
+            return methodDictionary;
+        }
+
+        public class IsX2 : IsMethod
+        {
+            public override bool execute(GameData game)
+            {
+                return game.isX2();
+            }
+        }
+    }
+
+
+}
--- Plock/baseGameMethodProperty.cs
+++ Plock/baseGameMethodProperty.cs
@@ -7,6 +7,7 @@ namespace Plock
 {
     using GameData = DummyGameProject.GameData;
 
+
     public class MethodWrapper
     {
         Method method;//execute(ゲームの更新用)
@@ -19,7 +20,7 @@ namespace Plock
 
         public void set(String code)
         {
-            method=GameMethodProperty.getDoMethodDictionary().Single(_method => code.Contains(_method.Key)).Value;
+            method = GameMethodProperty.getDoMethodDictionary().Single(_method => code.Contains(_method.Key)).Value;
             method.set(code);
         }
         public GameData execute(GameData game)
@@ -34,7 +35,7 @@ namespace Plock
         /// <returns></returns>
         public MoveTo getMoveTo(GameData game)
         {
-            return    method.getMoveTo(game);
+            return method.getMoveTo(game);
         }
 
         public bool isControlMethod()
@@ -88,50 +89,16 @@ namespace Plock
     }
 
 
-
-    class GameMethodProperty
+    class baseGameMethodProperty
     {
         public static Dictionary<string, Method> getDoMethodDictionary()
         {
             Dictionary<string, Method> methodDictionary = new Dictionary<string, Method>();
             methodDictionary.Add("もし", new IfMethod());
             methodDictionary.Add("}", new EmptyMethod());
-            //////////TODO:２．ゲームで使うメソッドのクラスを辞書に登録する/////////////
-            methodDictionary.Add("xを1増やす", new AddX());
-            methodDictionary.Add("xを1減らす", new MinusX());
-            methodDictionary.Add("ゲームの初期化", new Constructor());//コンストラクタもここで宣言する
             return methodDictionary;
         }
 
-        public class AddX : Method    //ダミーのゲームのメソッド
-        {
-            public override GameData execute(GameData game)
-            {
-                game.addX();
-                return game;
-            }
-        }
-        public class MinusX : Method    //ダミーのゲームのメソッド
-        {
-            public override GameData execute(GameData game)
-            {
-                game.minusX();
-                return game;
-            }
-        }
-
-        public class Constructor : Method    //ダミーのゲームのメソッド
-        {
-            public override GameData execute(GameData game)//GUIのプログラムから呼ぶ場合
-            {
-                return new GameData(3, 3);
-            }
-
-            public GameData execute()//最初の初期化
-            {
-                return new GameData(3, 3);
-            }
-        }
 
         public class IfMethod : ControlMethod
         {
@@ -150,30 +117,9 @@ namespace Plock
                 return game;
             }
         }
-
-
-
-        //////////bool値を返すメソッドも大体同じ要領で登録する/////////////
-        public static Dictionary<string, IsMethod> getIsMethodDictionary()
-        {
-            Dictionary<string, IsMethod> methodDictionary = new Dictionary<string, IsMethod>();
-
-            methodDictionary.Add("xが2なら", new IsX2());
-
-            return methodDictionary;
-        }
-
-        public class IsX2 : IsMethod
-        {
-            public override bool execute(GameData game)
-            {
-                return game.isX2();
-            }
-        }
     }
-
     public enum MoveTo
     {
-        NEXT,COLLERNEXT,COLLEE
+        NEXT, COLLERNEXT, COLLEE
     }
 }
